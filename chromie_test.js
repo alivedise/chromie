d0d8cb08313c9d0043c53c38cc7b96ee5d9ed823
alivedise <alegnadise@gmail.com>
2012-02-14 01:12:50 +0800

    pro

---
+++ chromie_test.js
@@ -0,0 +1,10 @@
+steal('funcunit/qunit','./chromie',function(){
+
+module("chromie");
+
+test("chromie testing works", function(){
+	ok(true,"an assert is run");
+});
+
+
+});
---
+++ synchronizor/_test.js
@@ -0,0 +1,10 @@
+steal('funcunit/qunit','./',function(){
+
+module("");
+
+test(" testing works", function(){
+	ok(true,"an assert is run");
+});
+
+
+});
